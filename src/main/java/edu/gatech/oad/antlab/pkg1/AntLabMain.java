3d2aa4890e3f5476644ced326823369b8fb040d1
sali45 <sali45@gatech.edu>
2016-01-28 16:59:49 -0500

    Took out GT ID # and put GT Login

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,14 +67,14 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("P3");
 		  toPrint += p3.toString("gburdell3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("Saqib Ali");
-                  toPrint += p4.toString("903046730");
-                  //Person5 replace P4 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Mohammed Saqib");
-                  toPrint += p5.toString("903108826");
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+          Person4 p4 = new Person4("Saqib Ali");
+          toPrint += p4.toString("sali70");
+          //Person5 replace P4 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("Mohammed Saqib");
+          toPrint += p5.toString("903108826");
 		  
         System.out.println(toPrint);
         
